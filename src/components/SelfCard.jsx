2623842688755db71c221f8005addc8556e5ac6a
Eya-khalfallah <eya.khalfallah@ensi-uma.tn>
2024-11-05 21:17:53 +0100

    Update CV

--- src/components/SelfCard.jsx
+++ src/components/SelfCard.jsx
@@ -88,7 +88,7 @@ const SelfCard = () => {
             </div>
             {/* Download CV Button */}
             <a
-              href="/public/Eya_Khalfallah_CV.pdf"
+              href="Eya_Khalfallah_CV.pdf"
               download="Eya_Khalfallah_CV.pdf"
               className="mt-4 w-full py-3 bg-[#c57284] rounded-full">
               {" "}
