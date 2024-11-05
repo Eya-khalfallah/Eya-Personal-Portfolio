a1725ed507790d2e6fac71b8c858a8119b4b946f
Eya-khalfallah <eya.khalfallah@ensi-uma.tn>
2024-11-05 19:03:36 +0100

    Adding animated cursor

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
-import { useState } from 'react'
-import './App.css'
-import HomePage from './home/HomePage'
+import "./App.css";
+import AnimatedCursor from "react-animated-cursor";
+import HomePage from "./home/HomePage";
 
 function App() {
-
   return (
-    <div className=' overflow-y-auto '>
-      
-       <HomePage /> 
+    <div className=" overflow-y-auto ">
+      <AnimatedCursor
+        innerSize={8}
+        outerSize={35}
+        innerScale={1}
+        outerScale={2}
+        outerAlpha={0}
+        hasBlendMode={true}
+        showSystemCursor={true}
+        innerStyle={{
+          backgroundColor: "#d67f92",
+        }}
+        outerStyle={{
+          border: "1px solid #f3f4f6",
+        }}
+      />
+      <HomePage />
     </div>
-      
-    
-  )
+  );
 }
 
-export default App
+export default App;
